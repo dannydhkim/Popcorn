043cb6b6b5b9a4954ba54616e4146f574c47fee3
Danny Kim <kr.dannydhkim@gmail.com>
2024-11-17 12:18:16 -0800

    sidebar and button working

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Sidebar from 'sidebar';
+
+const container = ReactDOM.createRoot(document.getElementById('popcorn-sidebar'));
+container.render(<Sidebar />, container);
